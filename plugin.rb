94072241aa527f8cb10a7118397197f59fa568b2
Kritphong Mongkhonvanit <kritphong@u.northwestern.edu>
2024-11-20 23:40:07 -0600

    Fix project URL in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: discourse-netlogo-embed
 # about: Display NetLogo models in embedded iframes
 # version: 0.0.1
-# url: https://github.com/kgmt-/discourse-netlogo-embed
+# url: https://github.com/kgmt0/discourse-netlogo-embed
 # authors: Kritphong Mongkhonvanit
 
 enabled_site_setting :netlogo_embed_enabled
